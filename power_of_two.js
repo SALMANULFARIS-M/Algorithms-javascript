4eef28e8d2833f672086a2f9b64050420ca91aec
Salmanulfaris-M <fsalmanul13@gmail.com>
2023-05-04 23:25:09 +0530

    powr of 2 method two

--- power_of_two.js
+++ power_of_two.js
@@ -1,15 +1,15 @@
-// function powerOfTwo(n) {
-//   if (n < 1) {
-//     return false;
-//   }
-//   while (n > 1) {
-//     if (n % 2 !== 0) {
-//       return false;
-//     }
-//     n = n / 2;
-//   }
-//   return true;
-// }
+function powerOfTwo(n) {
+  if (n < 1) {
+    return false;
+  }
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n = n / 2;
+  }
+  return true;
+}
 
 // Big O = O(logn)
 
